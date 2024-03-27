cd390f76a4302302401c3ac2a77f9b604e9b9d5e
Shubham Goel <shubham22490@iiitd.ac.in>
2024-03-27 22:00:19 +0530

    Added the functionality of seeing the users.

--- Extension/content.js
+++ Extension/content.js
@@ -1,13 +1,15 @@
 // Get the current URL
-
-const urlToExtract = window.location.href.split("/tasks")[0] + "/submissions/me";
+const baseURL = window.location.href.split("/tasks")[0];
+const urlToExtract = baseURL + "/submissions/me";
 
 var data = new Map();
 var parser = new DOMParser();
+let problems = [];
 var pageNum = 1;
-
+var currentPageHtml = parser.parseFromString(document.documentElement.outerHTML, "text/html");
 
 (async function() {
+    addColumn(currentPageHtml);
     while(true) {
         var url = urlToExtract + `?page=${pageNum}`;
         console.log(url);
@@ -30,8 +32,12 @@ var pageNum = 1;
 
         pageNum++;
     }
-    console.log(data);
-    updateTasks(document.documentElement.outerHTML);
+    const ACurl = baseURL + "/standings/json";
+    console.log(ACurl);
+    const resp = await fetch(ACurl);
+    const jsonData = await resp.json();
+    getACcount(jsonData);
+    updateTasks(currentPageHtml);
 })()
 
 // Function to find the 
@@ -49,14 +55,57 @@ function getData(parsedHtml) {
     }
 }
 
-// Function to update the tasks page
-function updateTasks(currentHtmlContent) {
-    // Parse the HTML content of the current webpage into a DOM document
-    var parsedCurrentHtml = parser.parseFromString(currentHtmlContent, "text/html");
+function getACcount(json){
+    let users = json.StandingsData;
+    problems = json.TaskInfo;
+    for(let i = 0; i < problems.length; i++) problems[i].count = 0;
+    for(let i = 0; i < users.length; i++){
+        let user = users[i];
+        for(let j = 0; j < problems.length; j++){
+            let result = user.TaskResults[problems[j].TaskScreenName];
+            if(result == undefined || result.Score == 0) continue;
+            problems[j].count++;
+        }
+    }
 
+    for(let i = 0; i < problems.length; i++) console.log(problems[i]);
+    // console.log(countAC);
+}
+
+function addColumn(parsedHTML){
+    
+    var table = parsedHTML.querySelector("table");
+    
+    var newHeaderCell = document.createElement("th");
+    newHeaderCell.textContent = "Accepted";
+    newHeaderCell.style.width = "8%";
+    newHeaderCell.classList.add("no-break");
+    newHeaderCell.classList.add("text-center");
+    table.rows[0].appendChild(newHeaderCell);
+
+    for(var i = 1; i < table.rows.length; i++){
+        let row = table.rows[i];
+        var cell = row.insertCell(-1);
+        cell.classList.add("text-center");
+        cell.classList.add("no-break");
+        cell.style.width = "8%";
+        cell.innerHTML = `<img src='https://codeforces.org/s/15108/images/icons/user.png' /> x${0}`;
+    }
+    console.log("Table: ", table);
+    var currentTable = document.querySelector("table");
+    currentTable.innerHTML = table.innerHTML;
+}
+
+// Function to update the tasks page
+function updateTasks(parsedCurrentHtml) {
     // Selecting all the rows one by one
     var elements = parsedCurrentHtml.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
+    var table = parsedCurrentHtml.querySelector("table");
+    console.log(table);
+    
     for(var i = 0; i < elements.length; i++){
+        let row = table.rows[i+1].cells[5];
+        row.innerHTML = `<img src='https://codeforces.org/s/15108/images/icons/user.png' /> x${problems[i].count}` ;
         var element = elements[i];
         var ques = element.getElementsByTagName("td")[1].textContent.trim();
         // var button = element.getElementsByTagName("td")
@@ -70,6 +119,6 @@ function updateTasks(currentHtmlContent) {
             }
         }
     }
-    // console.log(parsedCurrentHtml.documentElement.innerHTML)
-    document.documentElement.innerHTML = parsedCurrentHtml.documentElement.innerHTML;
+    var currentTable = document.querySelector("table");
+    currentTable.innerHTML = table.innerHTML;
 }
