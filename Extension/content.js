ad981011c48e1a48c1175ec2bafa600be1927815
Shubham Goel <shubham22490@iiitd.ac.in>
2024-03-27 18:22:12 +0530

    Fixed the bug to account all submissions.

--- Extension/content.js
+++ Extension/content.js
@@ -3,30 +3,39 @@
 const urlToExtract = window.location.href.split("/tasks")[0] + "/submissions/me";
 
 var data = new Map();
+var parser = new DOMParser();
+var pageNum = 1;
 
-fetch(urlToExtract)
-    .then(response => response.text())
-    .then(htmlContent => {
-        // Call a function to parse or extract information from the HTML content
-        parseHTML(htmlContent);
 
-        const currentHtmlContent = document.documentElement.outerHTML;
-        // Call a function to parse or extract information from the current HTML content
-        parseCurrentHTML(currentHtmlContent);
-    })
-    .catch(error => {
-        // Handle any errors that occur during the fetch request
-        console.error("Error fetching webpage:", error);
-    });
+(async function() {
+    while(true) {
+        var url = urlToExtract + `?page=${pageNum}`;
+        console.log(url);
+        try{
+            const response = await fetch(url);
+            const htmlContent = await response.text();
+            const parsedHTML = parser.parseFromString(htmlContent, 'text/html');
+            
+            const pager = parsedHTML.getElementsByClassName("pager");
+            if(pager.length == 0){
+                console.log("No more Pages");
+                break;
+            }
 
-function parseHTML(htmlContent) {
-    // Create a DOMParser object
-    var parser = new DOMParser();
+            getData(parsedHTML);
+        } catch(error){
+            console.log("Error: " + error);
+            break;
+        }
 
-    // Parse the HTML content into a DOM document
-    var parsedHtml = parser.parseFromString(htmlContent, "text/html");
+        pageNum++;
+    }
+    console.log(data);
+    updateTasks(document.documentElement.outerHTML);
+})()
 
-    // Select elements by their class using querySelectorAll
+// Function to find the 
+function getData(parsedHtml) {
     var elements = parsedHtml.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
     for (var i = 0; i < elements.length; i++) {
         var element = elements[i];
@@ -38,21 +47,14 @@ function parseHTML(htmlContent) {
             data.set(ques, state);
         }
     }
-
-    // console.log(data);
-
 }
 
-
-function parseCurrentHTML(currentHtmlContent) {
-    // Create a DOMParser object
-    var parser = new DOMParser();
-
+// Function to update the tasks page
+function updateTasks(currentHtmlContent) {
     // Parse the HTML content of the current webpage into a DOM document
     var parsedCurrentHtml = parser.parseFromString(currentHtmlContent, "text/html");
 
-    // Select elements or perform other parsing operations on the current webpage
-    // Example:
+    // Selecting all the rows one by one
     var elements = parsedCurrentHtml.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
     for(var i = 0; i < elements.length; i++){
         var element = elements[i];
